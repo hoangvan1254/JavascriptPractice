c741f256338f47b5101fe0dd4e9f7b8cedfeacc4
hoangvan1254 <phamthanh121384@gmail.com>
2018-06-20 15:57:03 +0700

    Add all my files

---
+++ calc.js
@@ -0,0 +1,19 @@
+var numField1 = document.getElementById('num1');
+var numField2 = document.getElementById('num2');
+var resultField = document.getElementById('result');
+var form = document.getElementById('XIsWhatPercentOfY');
+
+form.addEventListener('submit', function(event) {
+    if( !numField1.value || !numField2.value ) {
+        alert("Please enter your value");
+    } else {
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
+        var result = x / y;
+        var percent = result * 100;
+
+        resultField.innerText = "Answer:" + percent + "%";
+
+        event.preventDefault();
+    }
+});
